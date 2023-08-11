12ed74567888cddde03dfc303a3ce7a5f3064243
chineduCoded <chinedujohn17@gmail.com>
2023-08-11 02:41:41 +0000

    Session authentication implementation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,6 +23,12 @@ elif AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.before_request
@@ -33,16 +39,18 @@ def bef_req():
     if auth is None:
         pass
     else:
+        setattr(request, "current_user", auth.current_user(request))
         excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
-            request.current_user = auth.current_user(request)
-            if request.current_user is None:
+            if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
 
 
